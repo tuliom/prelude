8f80047258e21a535a46adfc3d797dd740b5ef0d
Tulio Magno Quites Machado Filho <tuliom@linux.vnet.ibm.com>
2012-09-17 08:40:26 -0300

    Disable ido-mode

--- prelude/prelude-editor.el
+++ prelude/prelude-editor.el
@@ -148,16 +148,6 @@
 ;; keep in mind known issues with zsh - see emacs wiki
 (setq tramp-default-method "ssh")
 
-;; ido-mode
-(ido-mode t)
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-max-prospects 10
-      ido-save-directory-list-file (concat prelude-savefile-dir "ido.hist")
-      ido-default-file-method 'selected-window)
-
 ;; auto-completion in minibuffer
 (icomplete-mode +1)
 
